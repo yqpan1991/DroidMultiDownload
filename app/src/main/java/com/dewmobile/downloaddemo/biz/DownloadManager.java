185073ff286cce0bbe9bbddab35cacac8a555651
yqpan1991 <shifengzhongdexiao@163.com>
2016-04-19 07:46:38 +0800

    yqpan :  delete useless code

--- app/src/main/java/com/dewmobile/downloaddemo/biz/DownloadManager.java
+++ app/src/main/java/com/dewmobile/downloaddemo/biz/DownloadManager.java
@@ -39,30 +39,6 @@ public class DownloadManager {
         return mInstance;
     }
 
-    public void download(String url){
-        if(TextUtils.isEmpty(url)){
-            return;
-        }
-        threadPoolManager.getDownloadThreadPool().execute(buildDownload(url));
-/*        //根据url，插入到数据库中即可
-        //数据库在插入成功后，向外通知插入数据库成功
-        //然后开始生成线程，下载文件的概要信息
-        //localpath
-        //fileSize等等信息
-        //下次再次进入时，读取数据库的状态即可
-
-        //文件的概要信息下载完成后
-        //生成下载的task，向外通知即可
-        //根据下载的url去查询即可
-        FileInfo info = new FileInfo();
-        info.url = url;
-        info.localPath = mContext.getCacheDir()+"/"+ "123.zip";
-        DownloadTask4 task = new DownloadTask4();
-        task.downloadInfo = info;
-        task.callback = callback;
-        new Thread(task).start();*/
-    }
-
     public void downloadByNormal(String url){
         if(TextUtils.isEmpty(url)){
             return;
@@ -70,29 +46,10 @@ public class DownloadManager {
         threadPoolManager.getPreDownloadThreadPool().execute(buildDownloadNormal(url));
     }
 
-
-
-    private Runnable buildDownload(String url){
-        Runnable runnable =  new InitDownloadInfoTask(mContext, url, new InitDownloadInfoTask.InitCallback() {
-            @Override
-            public void initSucceed(FileInfo fileInfo) {
-                DownloadTask2 downloadTask = new DownloadTask2();
-                downloadTask.downloadInfo = fileInfo;
-                downloadTask.run();
-            }
-
-            @Override
-            public void initFailed() {
-
-            }
-        });
-        return runnable;
-    }
-
     private Runnable buildDownloadNormal(String url){
         FileInfo fileInfo = new FileInfo();
         fileInfo.url = url;
-        Runnable runnable = new InitDownloadInfoTask2(fileInfo, new InitDownloadInfoTask2.InitCallback(){
+        Runnable runnable = new InitDownloadInfoTask(fileInfo, new InitDownloadInfoTask.InitCallback(){
 
             @Override
             public void onNewDownloadRecord(DownloadBean bean) {
--- app/src/main/java/com/dewmobile/downloaddemo/biz/InitDownloadInfoTask.java
+++ app/src/main/java/com/dewmobile/downloaddemo/biz/InitDownloadInfoTask.java
@@ -1,115 +1,72 @@
 package com.dewmobile.downloaddemo.biz;
 
-import android.content.Context;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.dewmobile.downloaddemo.MyApplication;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.dewmobile.downloaddemo.biz.db.DownloadBean;
+import com.dewmobile.downloaddemo.biz.db.DownloadDatabaseHelper;
 
 /**
  * 初始化下载的信息
  * 2. 检测当前的url是否在下载的数据库中，如果在，从数据库获取初始化文件的信息，检查状态位，如果是处于下载的队列中不做处理，否则，更改将其更改为排队下载中
- * 3. 如果没有在下载的记录中，生成完整的文件信息，然后生成一个下载的记录，插入到数据库中，然后从网络获取数据，如果数据获取完整，
+ * 3. 如果没有在下载的记录中，生成完整的文件信息，然后生成一个下载的记录，插入到数据库中
  * 同样插入到数据库中，然后将状态位更改为排队下载中
- * Created by panyongqiang on 16/4/8.
  */
-public class InitDownloadInfoTask implements Runnable {
+public class InitDownloadInfoTask implements Runnable{
 
-    private String url;
-    private Context mContext;
-    private FileInfo fileInfo;
-    private InitCallback mCallback;
+    private FileInfo mFileInfo;
+    //网络信息
+    private InitCallback mInitCallback;
 
-    private final String TAG = this.getClass().getSimpleName();
-
-    public InitDownloadInfoTask(Context context, String url, InitCallback callback) {
-        this.url = url;
-        mContext = context;
-        mCallback = callback;
+    public InitDownloadInfoTask(FileInfo fileInfo, InitCallback initCallback){
+        mFileInfo = fileInfo;
+        mInitCallback = initCallback;
     }
 
     @Override
     public void run() {
-        if (TextUtils.isEmpty(url)) {
-            //notify error
-            //insert into database,error
-            //向外广播即可，失败
+        if(TextUtils.isEmpty(mFileInfo.url)){
+            notifyOnInitError(mFileInfo);
             return;
         }
-        //
-        fileInfo = new FileInfo();
-        fileInfo.url = url;
-        //TODO ------need to check from db , if current filePath is exist,if exist,then add -1
-        fileInfo.generateLocalPath();
-        if (fileInfo.fileSize > 0) {
-            //insert into database
-            //init done
-        } else {
-            fetchInfoFromInternet();
-        }
-
-    }
-
-    private void fetchInfoFromInternet() {
-        //fetch from internet
-        URL url = null;
-        try {
-            url = new URL(fileInfo.url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        if (url == null) {
-            //notify error
-            mCallback.initFailed();
+        DownloadDatabaseHelper databaseHelper = DownloadDatabaseHelper.getInstance();
+        DownloadBean bean = databaseHelper.queryDownloadBean(mFileInfo.url);
+        if(bean != null){
+            notifyOnDownloadRecordExist(bean);
             return;
+        }else{
+            //TODO ------need to check from db , if current filePath is exist,if exist,then add -1
+            mFileInfo.generateLocalPath();
+            bean = new DownloadBean(mFileInfo);
+            bean.netType = 0;
+            bean.status = DownloadDatabaseHelper.STATUS_PENDING;
+            bean.currentSize = 0;
+            databaseHelper.addDownloadRecord(bean);
+            notifyNewDownloadRecord(bean);
         }
-        int contentLength = 0;
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setConnectTimeout(30 * 1000);
-            connection.setRequestMethod("GET");
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                contentLength = connection.getContentLength();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        if (contentLength <= 0) {
-            //error
-            mCallback.initFailed();
-            return;
-        } else {
-            fileInfo.fileSize = contentLength;
-            //insert into database
-            //notify parse succeed
-            if(mCallback != null){
-                mCallback.initSucceed(fileInfo);
-            }
-
-        }
-        Log.e(TAG, fileInfo.toString());
-
     }
 
-    private FileInfo checkFromDatabase() {
-        return null;
+    private void notifyOnDownloadRecordExist(DownloadBean bean){
+        if(mInitCallback != null){
+            mInitCallback.onDownloadRecordExist(bean);
+        }
+    }
+    private void notifyOnInitError(FileInfo fileInfo){
+        if(mInitCallback != null){
+            mInitCallback.onInitError(fileInfo);
+        }
     }
 
-    //需要向外广播出去当前文件的状态
-    //在完成后，向外通知当前文件的状态
-
-    public interface  InitCallback{
-        void initSucceed(FileInfo fileInfo);
-        void initFailed();
+    private void notifyNewDownloadRecord(DownloadBean bean){
+        if(mInitCallback != null){
+            mInitCallback.onNewDownloadRecord(bean);
+        }
     }
+
+    public interface InitCallback{
+        void onNewDownloadRecord(DownloadBean bean);
+        void onDownloadRecordExist(DownloadBean bean);
+        void onInitError(FileInfo fileInfo);
+    }
+
 }
